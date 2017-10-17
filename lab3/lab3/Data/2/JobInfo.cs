612b52bce1142858541e2e923bde5b0c483927e0
robertbutacu <robert.butacu@yahoo.com>
2017-10-17 09:38:30 +0300

    Added solution, no tests.

---
+++ lab3/lab3/Data/2/JobInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3.Data._2
+{
+    class JobInfo
+    {
+        public Guid Id { get; set; }
+        public String Title { get; private set; }
+        public int MinimumYearsOfExperience { get; private set; }
+        public String Requirements { get; private set; }
+        public Double Salary {get; private set; }
+
+        public JobInfo(String title, int minYearsOfReq, String req, double salary)
+        {
+            if (minYearsOfReq < 0 || salary < 0)
+                throw new ArgumentException("Invalid arguments");
+
+            this.Id = Guid.NewGuid();
+            this.Title = title;
+            this.MinimumYearsOfExperience = minYearsOfReq;
+            this.Requirements = req;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ lab3/lab3/Data/2/JobInfoRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3.Data._2
+{
+    class JobInfoRepository
+    {
+        public List<JobInfo> jobsInfo { get; set; }
+
+        public JobInfoRepository()
+        {
+            jobsInfo.Add(new JobInfo("Scala", 0, "passionate", 1000));
+            jobsInfo.Add(new JobInfo("Scala", 1, "Akka", 2000));
+            jobsInfo.Add(new JobInfo("Scala", 2, "FP", 3000));
+            jobsInfo.Add(new JobInfo("Scala", 3, "BigData", 4000));
+        }
+
+        public List<JobInfo> GetJobByTitle(string jobTitle)
+        {
+            return jobsInfo.FindAll(j => j.Title == jobTitle);
+        }
+
+        public List<JobInfo> FindAllJobs()
+        {
+            return jobsInfo;
+        }
+
+        public void AddJob(JobInfo jobInfo)
+        {
+            jobsInfo.Add(jobInfo);
+        }
+
+        public List<JobInfo> GetJobByPosition(int position)
+        {
+            return jobsInfo.FindAll(e => e.MinimumYearsOfExperience == position);
+        }
+
+        public void RemoveJobsWithTitle(string jobTitle)
+        {
+            jobsInfo.RemoveAll(e => e.Title == jobTitle);
+        }
+
+        public List<JobInfo> GetJobsWithSalaryHigherThan(int limit)
+        {
+            return jobsInfo.FindAll(e => e.Salary > limit);
+        }
+
+
+    }
+}
