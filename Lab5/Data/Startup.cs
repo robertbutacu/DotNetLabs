e41e4998a9df6bed3a094badd7a0cc41a55d65a1
robertbutacu <robert.butacu@yahoo.com>
2017-10-31 08:19:41 +0200

    added 3 projects.

--- Lab5/Data/Startup.cs
+++ Lab5/Data/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Lab5
+namespace Data
 {
     public class Startup
     {
